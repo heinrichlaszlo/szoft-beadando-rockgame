f7eabf32e87e59326217c440c4b8001ff5656846
heinrichlaszlo <heinrichlaszlo99@gmail.com>
2021-05-15 16:20:38 +0200

    Create JpaInitializer.java

---
+++ src/main/java/util/guice/JpaInitializer.java
@@ -0,0 +1,23 @@
+package util.guice;
+
+import com.google.inject.persist.PersistService;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+//már a játék elején megnyitja az adatbázist
+/**
+ * This utility class initializes JPA.
+ */
+@Singleton
+public class JpaInitializer {
+
+    /**
+     * Starts the persistence service.
+     * @param persistService the persistence service
+     */
+    @Inject
+    public JpaInitializer (PersistService persistService) {
+        persistService.start();
+    }
+
+}
