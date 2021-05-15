a7d2f77805d80bda13e5e6dd3e311ec2c94ac69f
heinrichlaszlo <heinrichlaszlo99@gmail.com>
2021-05-15 16:14:38 +0200

    Create GameResultDao.java

---
+++ src/main/java/game/results/GameResultDao.java
@@ -0,0 +1,32 @@
+package game.results;
+
+import util.jpa.GenericJpaDao;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+/**
+ * DAO class for the {@link GameResult} entity.
+ */
+public class GameResultDao extends GenericJpaDao<GameResult> {
+
+    /**
+     * The constructor that accepts no arguments.
+     */
+    public GameResultDao() {
+        super(GameResult.class);
+    }
+
+    /**
+     * Returns the list of all results with respect to the number of steps
+     * and the time spent for playing the game.
+     *
+     * @return the list of all results with respect to the number of steps
+     * and the time spent for playing the game
+     */
+    @Transactional
+    public List<GameResult> findAll() {
+        return entityManager.createQuery("SELECT r FROM GameResult r order by r.duration asc", GameResult.class)
+                .getResultList();
+    }
+}
