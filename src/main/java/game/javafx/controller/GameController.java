159f6a72c8f240401c74ac384a12d79d808d901d
heinrichlaszlo <heinrichlaszlo99@gmail.com>
2021-05-15 15:37:43 +0200

    Create GameController.java

---
+++ src/main/java/game/javafx/controller/GameController.java
@@ -0,0 +1,43 @@
+package game.javafx.controller;
+
+import com.sun.javafx.logging.Logger;
+import game.model.GameModel;
+import game.results.GameResult;
+import game.results.GameResultDao;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.time.DurationFormatUtils;
+
+import java.time.Instant;
+//import org.apache.commons.lang.time.DurationFormatUtils;
+
+
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Slf4j
+public class GameController {
+}
