0527d54c71087b08712a47e9b0bf7c2e165cb47a
heinrichlaszlo <heinrichlaszlo99@gmail.com>
2021-05-15 16:11:38 +0200

    Create StoneGameApp.java

---
+++ src/main/java/game/javafx/StoneGameApp.java
@@ -0,0 +1,24 @@
+package game.javafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.inject.Inject;
+
+public class StoneGameApp extends Application {
+    @Inject
+    private FXMLLoader fxmlLoader;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/launcher.fxml"));
+        primaryStage.setTitle("Köves Gameszko");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+}
